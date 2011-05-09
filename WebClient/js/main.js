d0b76db22691c6322c75750f877bbfd6ef007ff3
Jim Cowart <ashbylane@gmail.com>
2011-05-08 22:06:51 -0400

    Added mocked ajax calls.  I still need to make the repository object an actual function object....

--- WebClient/js/main.js
+++ WebClient/js/main.js
@@ -1,23 +1,74 @@
+/*amplify.request.define( "submitOrder", "ajax", {
+    url: "/order/{orderNumber}",
+    dataType: "json",
+    type: "PUT",
+    contentType : "application/json"
+});*/
+amplify.request.define( "submitOrder", function( settings ) {
+    settings.success("Order Received");
+});
+
+/*amplify.request.define("getMenuItems", "ajax", {
+    url: "/menu",
+    dataType: "json",
+    type: "GET",
+    contentType : "application/json"
+});*/
+amplify.request.define( "getMenuItems", function( settings ) {
+	settings.success([
+		new MenuItem({itemId:1, description: "Burger", imageSource: "./img/burger.png"}),
+        new MenuItem({itemId:2, description: "Fries", imageSource: "./img/fries.png"}),
+        new MenuItem({itemId:3, description: "Drink", imageSource: "./img/drink.png"}),
+        new MenuItem({itemId:4, description: "Shake", imageSource: "./img/shake.png"})
+	]);
+});
+
+/*amplify.request.define("getOrderStatus", "ajax", {
+    url: "/orderStatus",
+    dataType: "json",
+    type: "POST",
+    contentType : "application/json"
+});*/
+amplify.request.define("getOrderStatus", function( settings ) {
+    var testData = [];
+    var i = 0;
+    for(i; i < window.viewModel.orders().length; i++) {
+        testData.push({
+                        OrderNumber: window.viewModel.orders()[i].orderNumber(),
+                        Status: "It's in the Queue"
+                      });
+    }
+    settings.success(testData);
+});
+
 var repository = {
     getUniqueId: function() {
         return Math.floor(Math.random() * 1000) * Math.floor(Math.random() * 1000);
     },
     
-    getMenuList: function() {
-        return [
-            new MenuItem({itemId:1, description: "Burger", imageSource: "./img/burger.png"}),
-            new MenuItem({itemId:2, description: "Fries", imageSource: "./img/fries.png"}),
-            new MenuItem({itemId:3, description: "Drink", imageSource: "./img/drink.png"}),
-            new MenuItem({itemId:4, description: "Shake", imageSource: "./img/shake.png"})
-        ];
+    getMenuList: function(callback) {
+        amplify.request("getMenuItems", callback);
     },
 
-    submitOrder: function() {
-        //TODO: tie in amplify.js to submit ajax call
+    submitOrder: function(order) {
+        var createOrderCommand = {
+                                    Id: order.orderNumber(),
+                                    CustomerName: order.customerName(),
+                                    ItemIds: order.menuItems()
+                                 };
+        var data = { request: JSON.stringify(createOrderCommand) };
+        amplify.request("submitOrder", data, function() { window.viewModel.updateOrderStatus(order.orderNumber(), "Order Received"); });
     },
 
-    getOrderStatus: function(orderNumber) {
-        //TODO: tie in amplify.js to make a request for all pending orders, etc.
+    getOrderStatus: function(orderNumbers) {
+        var ords = { OrderNumbers: orderNumbers };
+        var data = { request: JSON.stringify(ords) };
+        amplify.request("getOrderStatus", data, function(data) {
+            var x = 0;
+            for(x; x < data.length; x ++) {
+                window.viewModel.updateOrderStatus(data[x].OrderNumber, data[x].Status);
+            }
+        });
     }
 };
 
@@ -39,7 +90,7 @@ var Order = function(options) {
     this.orderNumber = new ko.observable(opt.orderNumber || repository.getUniqueId());
     this.customerName = new ko.observable(opt.customerName || "");
     this.menuItems = new ko.observableArray(opt.menuItems || []);
-    this.status = new ko.observable("Pending");
+    this.status = new ko.observable(opt.status || "Pending");
     this.addOrderedItem = function(menuItem) {
         var i = 0;
         for(i; i < this.menuItems().length;i++) {
@@ -82,13 +133,14 @@ var Order = function(options) {
 };
 
 window.ViewModel = function() {
+
     this.orderToPlace = new Order();
 
     this.orders = new ko.observableArray([]);
 
     this.validationMessages = new ko.observableArray([{msg: "You haven't placed an order yet..."}]);
 
-    this.menu = new ko.observableArray(repository.getMenuList());
+    this.menu = new ko.observableArray([]);
 
     this.placeOrder = function() {
         var valid = true;
@@ -104,11 +156,13 @@ window.ViewModel = function() {
         }
 
         if(valid)  {
-            this.orders.push(new Order({
-                                            orderNumber: this.orderToPlace.orderNumber(),
-                                            customerName: this.orderToPlace.customerName(),
-                                            menuItems: this.orderToPlace.menuItems()
-                                            }));
+            var newOrder = new Order({
+                                        orderNumber: this.orderToPlace.orderNumber(),
+                                        customerName: this.orderToPlace.customerName(),
+                                        menuItems: this.orderToPlace.menuItems()
+                                     });
+            this.orders.push(newOrder);
+            repository.submitOrder(newOrder);
             this.clearOrder();
         }
     };
@@ -121,6 +175,39 @@ window.ViewModel = function() {
     this.cancelOrder = function() {
         //TODO: implement this.....
     };
+
+    this.loadMenu = function() {
+        var menu = this.menu;
+        repository.getMenuList(function(data) {
+            var i = 0;
+            for(i; i < data.length; i++) {
+                menu.push(data[i]);
+            }
+        });
+    };
+
+    this.updateOrderStatus = function(orderNumber, status) {
+        var i = 0;
+        for(i; i < this.orders().length; i++) {
+            if(this.orders()[i].orderNumber() === orderNumber) {
+                this.orders()[i].status(status);
+                break;
+            }
+        }
+    };
+
+    this.startPolling = function() {
+        setInterval(function() {
+            var ordNums = [];
+            var i = 0;
+            for(i; i < window.viewModel.orders().length; i++) {
+                ordNums.push(window.viewModel.orders()[i].orderNumber());
+            }
+            repository.getOrderStatus(ordNums);
+        }, 5000);
+    };
+
+    this.startPolling();
 };
 
 $(function(){
@@ -130,4 +217,5 @@ $(function(){
   });
   window.viewModel = new window.ViewModel();
   ko.applyBindings(window.viewModel);
+  window.viewModel.loadMenu();
 });
