5716ddc1187f12d14ae282fbcc0c4554c3ecff47
Alex Robson <asrobson@gmail.com>
2011-05-10 22:37:34 -0400

    Fixing file server problems

--- src/shortorder.http/Initializer.cs
+++ src/shortorder.http/Initializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using hyperstack;
 using hyperstack.Owin.Http;
@@ -16,56 +17,60 @@ namespace shortorder.http
     {
         public void Initialize()
         {
-            DaemonAssimilation.RunDaemon( Assimilate
-                                .Initialize()
-                                .RocketSockets( x => x.UseDefaultEndpoint() )
-                                .HyperStack( x => x
-                                                      .ConfigureHost( c => c
-                                                          .BasePath( @"..\..\..\WebClientOrderForm" )
-                                                          .AddViewSearchFolder( "template" )
-                                                      )
-                                                      .RegisterApplications( routes => routes
-                                                            .DefineApplication( request => request.RequestUri.EndsWith( "fav.ico" ), ( request, response, exception ) => 
-                                                            {
-                                                                response( HttpStatus.NoContent.ToString(), new Dictionary<string, string>(), ( bytes, error, continuation ) => () => { } ); 
-                                                            } )
-                                                            .DefineApplication<FileServer>( request =>
-                                                            {
-                                                                var last = request.PathSegments.LastOrDefault() ?? "";
-                                                                return last.Contains( "." );
-                                                            } )
-                                                            .DefineApplication<IdService>( request => 
-                                                                "uniqueid".Equals( request.PathSegments.FirstOrDefault() ) 
-                                                                && request.Method.Equals( "GET" ) )
-                                                            .DefineApplication<MenuItemList>( request => 
-                                                                "menu".Equals( request.PathSegments.FirstOrDefault() )
-                                                                && request.Method.Equals( "GET" ) )
-                                                            .DefineApplication<NewMenuItem>( request =>
-                                                                "menuitem".Equals( request.PathSegments.FirstOrDefault() ) 
-                                                                && request.PathSegments.Count == 2
-                                                                && request.Method.Equals( "PUT" ) )
-                                                            .DefineApplication<UpdateMenuItem>( request =>
-                                                                "menuitem".Equals( request.PathSegments.FirstOrDefault() ) 
-                                                                && request.PathSegments.Count == 2
-                                                                && request.Method.Equals( "POST" ) )
-                                                            .DefineApplication<NewOrder>( request =>
-                                                                "order".Equals( request.PathSegments.FirstOrDefault() )
-                                                                && request.Method.Equals( "PUT" ) )
-                                                            .DefineApplication<OrderList>( request =>
-                                                                "order".Equals( request.PathSegments.FirstOrDefault() )
-                                                                && request.PathSegments.Count == 1
-                                                                && request.Method.Equals( "GET" ) )
-                                                            .DefineApplication<OrderStatus>( request =>
-                                                                "order".Equals( request.PathSegments.FirstOrDefault() )
-                                                                && request.PathSegments.Count == 2
-                                                                && request.Method.Equals( "GET" ) )
-                                                            .DefineApplication<FileServer>( request => 
-                                                                request.PathSegments.Count == 0 )
-                                                        )
-                                            )
-                                .AddConsoleLogger<HostService>( x => x.Debug().MessageLayout( m => m.Message().Newline() ) )
-                                .Rabbit( x => x.AddBroker( b => b.Defaults() ).EnrollAsMeshNode( true ) )
-                                .Daemon( x => x.Name( "shortstack.http" ) ) );
+            Assimilate
+                .Initialize()
+                .RocketSockets( x => x.UseDefaultEndpoint() )
+                .HyperStack( x => x
+                                        .ConfigureHost( c => c
+                                            .BasePath( @"..\..\..\WebClientOrderForm" )
+                                            .AddViewSearchFolder( "template" )
+                                        )
+                                        .RegisterApplications( routes => routes
+                                            .DefineApplication( request => request.RequestUri.EndsWith( "fav.ico" ), ( request, response, exception ) => 
+                                            {
+                                                response( HttpStatus.NoContent.ToString(), new Dictionary<string, string>(), ( bytes, error, continuation ) => () => { } ); 
+                                            } )
+                                            .DefineApplication<FileServer>( request =>
+                                            {
+                                                var last = request.PathSegments.LastOrDefault() ?? "";
+                                                return last.Contains( "." ) || request.RequestUri.Equals( "/" );
+                                            } )
+                                            .DefineApplication<IdService>( request => 
+                                                "uniqueid".Equals( request.PathSegments.FirstOrDefault() ) 
+                                                && request.Method.Equals( "GET" ) )
+                                            .DefineApplication<MenuItemList>( request => 
+                                                "menu".Equals( request.PathSegments.FirstOrDefault() )
+                                                && request.Method.Equals( "GET" ) )
+                                            .DefineApplication<NewMenuItem>( request =>
+                                                "menuitem".Equals( request.PathSegments.FirstOrDefault() ) 
+                                                && request.PathSegments.Count == 2
+                                                && request.Method.Equals( "PUT" ) )
+                                            .DefineApplication<UpdateMenuItem>( request =>
+                                                "menuitem".Equals( request.PathSegments.FirstOrDefault() ) 
+                                                && request.PathSegments.Count == 2
+                                                && request.Method.Equals( "POST" ) )
+                                            .DefineApplication<NewOrder>( request =>
+                                                "order".Equals( request.PathSegments.FirstOrDefault() )
+                                                && request.Method.Equals( "PUT" ) )
+                                            .DefineApplication<OrderList>( request =>
+                                                "order".Equals( request.PathSegments.FirstOrDefault() )
+                                                && request.PathSegments.Count == 1
+                                                && request.Method.Equals( "GET" ) )
+                                            .DefineApplication<OrderStatus>( request =>
+                                                "order".Equals( request.PathSegments.FirstOrDefault() )
+                                                && request.PathSegments.Count == 2
+                                                && request.Method.Equals( "GET" ) )
+                                            .DefineApplication( request => true, ( request, response, exception ) => 
+                                            {
+                                                response( HttpStatus.NoContent.ToString(), new Dictionary<string, string>(), ( bytes, error, continuation ) => () => { } ); 
+                                            } )
+                                        )
+                            )
+                .AddConsoleLogger<HostService>( x => x.Debug().MessageLayout( m => m.Message().Newline() ) )
+                .Rabbit( x => x.AddBroker( b => b.Defaults() ).EnrollAsMeshNode( true ) )
+                .Daemon( x => x.Name( "shortstack.http" ) )
+                .Dependencies( x => x.For<FileServer>().Use<FileServer>().AsSingleton() )
+                .RunDaemon();
         }
     }
 }
